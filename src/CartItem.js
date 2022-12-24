2265e9c13ac7b6146e01a41011362b4a9f680450
vikramkbgs@gmail.com <vikramkbgs@gmail.com>
2022-12-24 20:20:42 +0530

    mangaing state

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,15 +1,17 @@
 import React from 'react';
 
-class CartItem extends React.Component {
-  render () {
-    console.log("this.props", this.props);
-    const { price, title, qty } = this.props.product;
-    
+const CartItem = (props) => {
+    const { price, title, qty } = props.product;
+    const { 
+      product, 
+      onIncreaseQuantity, 
+      onDecreaseQuantity, 
+      onDeleteProduct } = props;
     return (
       <div className="cart-item">
-        {this.props.jsx}
+        {props.jsx}
         <div className="left-block">
-          <img style={styles.image} alt="" />
+          <img style={styles.image} alt="" src={product.img} />
         </div>
         <div className="right-block">
           <div style={{ fontSize: 25 }}>{title}</div>
@@ -21,24 +23,24 @@ class CartItem extends React.Component {
               alt="increase"
               className="action-icons"
               src="https://www.pngmart.com/files/21/Add-Button-PNG-Transparent.png"
-              onClick={() => this.props.onIncreaseQuantity(this.props.product)}
+              onClick={() => onIncreaseQuantity(product)}
             />
             <img
               alt="decrease"
               className="action-icons"
               src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRk_QZe0oYEp6-QEsMGseYQ1X2vEaFb_SDz6zfh9Ar8et4-f6fgm4G6jivSlDDNZB2AfAk&usqp=CAU"
-              onClick={() => this.props.onDecreaseQuantity(this.props.product)}
+              onClick={() => onDecreaseQuantity(product)}
             />
             <img
               alt="delete"
               className="action-icons"
               src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRf1KlcHYRhmQBFbU1GgWNlS0dn4_2AF-K0cTkP8B-FmpRa62evYhVMfY0sJH7O7GisVxE&usqp=CAU"
+              onClick={() => onDeleteProduct(product.id)}
             />
           </div>
         </div>
       </div>
     );
-  }
 }
 
 const styles = {
